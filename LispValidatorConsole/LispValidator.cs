c6ece8aebdb6d53dd13ee415ca2864484c117737
Ken Willa <kwilla822@gmail.com>
2020-04-09 12:48:29 -0400

    Initial check-in of module Validation

---
+++ LispValidatorConsole/LispValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace Validation
+{
+    public class LispValidator
+    {
+        public static bool Validate(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            var result = 0;
+            foreach (var chr in input)
+            {
+                if (chr == '(') result += 1;
+                if (chr == ')') result -= 1;
+
+                if (result < 0)
+                    break;
+            }
+
+            return result == 0;
+        }
+    }
+}
---
+++ LispValidatorConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter LISP Code to Validate/n");
+            Console.WriteLine();
+
+            var input = Console.ReadLine();
+            var result = Validation.LispValidator.Validate(input);
+
+            Console.WriteLine();
+            Console.WriteLine("LISP Code is: " + (result ? "Valid" : "Invalid/n"));
+        }
+    }
+}
